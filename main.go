f0fcb49e571905d650b2a990379f8128cb867302
V <vera.worri@protonmail.com>
2021-11-07 11:24:53 -0500

    initial commmit

---
+++ main.go
@@ -0,0 +1,13 @@
+// Copyright 2017 Zack Guo <zack.y.guo@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT license that can
+// be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import "github.con/vworri/side-car/tui"
+
+func main() {
+	tui.StartTui()
+}
